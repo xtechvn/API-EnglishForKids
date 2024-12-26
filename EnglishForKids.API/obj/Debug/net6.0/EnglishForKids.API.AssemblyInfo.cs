263a23fb9ba68d50084c4765a23798b89b25127d
Đăng Tây <nguyendang01092003@gmail.com>
2024-12-26 10:23:17 +0700

    UpdateApi26

--- EnglishForKids.API/obj/Debug/net6.0/EnglishForKids.API.AssemblyInfo.cs
+++ EnglishForKids.API/obj/Debug/net6.0/EnglishForKids.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnglishForKids.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cb8a50b400d04466e3b621ea6fbf8fb4007df10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5bae6c1571c05b7d7f99bc4d66dc81da70c6620")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnglishForKids.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnglishForKids.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
